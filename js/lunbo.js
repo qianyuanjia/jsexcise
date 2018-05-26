f9966ad1c4a4f08d43013760c4151d7d35112524
QianYuanjia <qianyuanjia@qq.com>
2018-05-26 11:08:07 +0800

    edit

--- js/lunbo.js
+++ js/lunbo.js
@@ -1,13 +1,15 @@
-let picNum=$('.pics>li').length;
-$('.pics').width((picNum+1)*430);
-$('.pics>li:first').clone(true).appendTo('.pics');
-let n=0;
-setInterval(() => {
-	n++;
-	$('.pics').animate({'margin-left':-430*n},300,function(){
-		if(n===picNum){
-			n=0;
-			$('.pics').css('margin-left',0);
-		}
-	});
-},2000);
+$(function(){
+	let picNum=$('.pics>li').length;
+	$('.pics').width((picNum+1)*430);
+	$('.pics>li:first').clone(true).appendTo('.pics');
+	let n=0;
+	setInterval(() => {
+		n++;
+		$('.pics').animate({'margin-left':-430*n},300,function(){
+			if(n===picNum){
+				n=0;
+				$('.pics').css('margin-left',0);
+			}
+		});
+	},2000);
+})
